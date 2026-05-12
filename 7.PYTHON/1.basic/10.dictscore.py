c915d6b6e2411a84c995bd7a03b773c1598cbe76
shinhjin02 <shinhjin02@gmail.com>
2026-05-12 09:18:10 +0900

    python 기본 함수

--- 7.PYTHON/1.basic/10.dictscore.py
+++ 7.PYTHON/1.basic/10.dictscore.py
@@ -1,21 +1,23 @@
 students = {
-    "민수" : 84,
-    "지은" : 23,
-    "서준" : 77,
-    "하린" : 91,
-    "도윤" : 65,
-    "예린" : 48,
-    "지후" : 12,
-    "수아" : 99,
-    "현우" : 53,
-    "유진" : 71 
- }
+    "김민준": 87,
+    "이서연": 92,
+    "박지훈": 78,
+    "최유진": 95,
+    "정도현": 81,
+    "한지민": 89,
+    "윤태호": 73,
+    "강수아": 98,
+    "오세훈": 84,
+    "백예린": 91
+}
 
 print(students)
 
 def get_a_student(students):
     a_students = []
-    for name, scroe in students.items():
+    for name, score in students.items(): # dict의 요소를 하나씩 가져옴 (items())
         if score >= 90:
             a_students.append(name)
     return a_students
+
+print("A등급 학생: ", get_a_student(students))
