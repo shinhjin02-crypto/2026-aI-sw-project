9ba54f005d713c83b9ab1b099677b72eaddb913b
shinhjin02 <shinhjin02@gmail.com>
2026-05-13 09:27:21 +0900

    flask 시작

--- 7.PYTHON/7.flask/3.users.py
+++ 7.PYTHON/7.flask/3.users.py
@@ -4,32 +4,26 @@ app = Flask(__name__)
 
 users = [
     {'name': 'Alice', 'age': 25, 'phone': '123-456-7890'},
-    {'name': 'Bob', 'age': 30, 'phone': '123-444-7890'},
-    {'name': 'Charlie', 'age': 27, 'phone': '123-555-7890'}
+    {'name': 'Bob', 'age': 30, 'phone': '123-555-7890'},
+    {'name': 'Charlie', 'age': 27, 'phone': '123-777-7890'}
 ]
+# 파이썬의 리스트 폼, 각각의 리스트에는 딕셔너리
 
 @app.route('/')
 def main():
-    # list/dict -> JSON 형태로 변환
-    return jsonify(users)
+    return jsonify(users)  # 우리의 백엔드 list/dict 구조를 웹이 좋아하는 JSON 포멧으로 변환해서 보내줌
 
-# user 오타 수정
 @app.route('/user/<name>')
 def get_user_by_name(name):
-
-    print("사용자 입력값:", name)
-
+    print("사용자입력값: ", name)
     user = None
-
     for u in users:
-        if u['name'] == name:
+        if u['name'].lower() == name.lower():
             user = u
 
     if user:
-        # uesr -> user 오타 수정
         return jsonify(user)
     else:
-        # 문자열은 따옴표 필요
         return jsonify({"message": "사용자를 찾지 못했습니다."})
 
 if __name__ == '__main__':
