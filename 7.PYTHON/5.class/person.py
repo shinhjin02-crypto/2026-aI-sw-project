c367845b8ebb88197cf365a90317f9c2956c061a
신혜진 <shinhjin02@gmail.com>
2026-05-13 17:13:23 +0900

    python 페이지에서 정보 불러오기

--- 7.PYTHON/5.class/person.py
+++ 7.PYTHON/5.class/person.py
@@ -7,9 +7,13 @@ class Person:
         print(f"안녕하세요, 저는 {self.name} 입니다.")
 
     def study(self, subject):
-        print(f"{self.name} 는 {subject}를 공부하고 있습니다.")
+        print(f"{self.name} 는 {subject} 를 공부하고 있습니다.")
 
 person1 = Person("Alice", 25)
 person2 = Person("Bob", 27)
 
-person
+person1.greet()
+person2.greet()
+
+person1.study("Python")
+person2.study("영어")
