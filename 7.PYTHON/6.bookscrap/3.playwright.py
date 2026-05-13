bcfbb4d81f8006382529c40186bb7ec282a96812
shinhjin02 <shinhjin02@gmail.com>
2026-05-13 09:23:45 +0900

    python 웹페이지 스크랩

--- 7.PYTHON/6.bookscrap/3.playwright.py
+++ 7.PYTHON/6.bookscrap/3.playwright.py
@@ -1,19 +1,19 @@
-#pip install playwright
-#playwright install
+# pip install playwright
+# playwright install  <-- 각종 브라우저 드라이버를 관리
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    #크롬을 실행한다
+    # 크롬을 실행한다
     browser = p.chromium.launch(headless=False)
 
-    #빈 페이지를 띄운다
+    # 빈 페이지를 띄운다
     page = browser.new_page()
 
-    #원하는 사이트로 가게한다
+    # 원하는 사이트로 가게한다
     page.goto("https://www.naver.com")
-
+    
     print(page.title())
 
     page.screenshot(path="naver.png")
 
-    input("엔터를 누르면 종료됩니다.")
+    input("엔터를 누르면 종료됩니다")
