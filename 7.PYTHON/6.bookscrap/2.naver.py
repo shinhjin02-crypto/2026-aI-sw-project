7184c16ea81e54d6ea068ebe0f89091982008c68
shinhjin02 <shinhjin02@gmail.com>
2026-05-13 09:23:21 +0900

    python 웹페이지 스크랩

--- 7.PYTHON/6.bookscrap/2.naver.py
+++ 7.PYTHON/6.bookscrap/2.naver.py
@@ -1,1 +1,14 @@
-resp = re
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "https://www.naver.com"
+
+resp = requests.get(url)
+resp.encoding = "utf-8"  # 유니코드 글자로 인식시켜서 깨지는 글자 제거
+
+soup = BeautifulSoup(resp.text, "html.parser")
+print(soup)
+
+news = soup.select(".MediaNewsView-module__desc_list___uQ3r1")
+print(news)
