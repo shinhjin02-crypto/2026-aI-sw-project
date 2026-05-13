a3278d780906fa62a55d88b31b53589d219782b8
shinhjin02 <shinhjin02@gmail.com>
2026-05-13 09:24:44 +0900

    python 웹페이지 스크랩

--- 7.PYTHON/6.bookscrap/5.playwright_naver.py
+++ 7.PYTHON/6.bookscrap/5.playwright_naver.py
@@ -5,4 +5,17 @@ with sync_playwright() as p:
     page = browser.new_page()
     page.goto("https://news.naver.com/section/105")
 
-    input("엔터 누르면 종료")
+    headlines = page.locator(".section_article.as_headline a.sa_text_title")
+    print("헤드라인 갯수: ", headlines.count())
+    for i in range(headlines.count()):
+        news = headlines.nth(i)
+
+        # 제목 가져오기
+        title = news.inner_text().strip()
+
+        # 링크 가져오기
+        href = news.get_attribute('href')
+
+        print(f"{i+1}. {title}\n   {href}")
+
+    input("엔터")
