31203884b4f523135087eebaf3f3ff4a994184be
신혜진 <shinhjin02@gmail.com>
2026-05-29 13:25:41 +0900

    랭체인 Chaining / Runnable 등

--- 10.LangChain/5.status/6.tripplanner.py
+++ 10.LangChain/5.status/6.tripplanner.py
@@ -4,3 +4,13 @@
 #          -> 호텔 추천
 # 사용자 입력의 OO을 보고, 시간표/동선/교통수단 vs 음식/관광지/호텔
 # RunnableParallel, RunnableBranch
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableBranch
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini")
