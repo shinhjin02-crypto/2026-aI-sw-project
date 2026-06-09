62828c26bc7f2cd1395e307a271a07638cc91e34
신혜진 <shinhjin02@gmail.com>
2026-06-09 10:25:12 +0900

    gpt2 및 이미지 모델

--- 13.LOCAL/18.hf_image2.py
+++ 13.LOCAL/18.hf_image2.py
@@ -1,15 +1,26 @@
-# pip install -U diffusers transformers accelerate sentencepiece torch safetensors
+# pip install -U diffusers transformers accelerate sentencepiece torch safetensors python-dotenv torchvision
 
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+from dotenv import load_dotenv
 from diffusers import FluxPipeline
 import torch
 import time
 
+load_dotenv()
+
 MODEL_ID = "black-forest-labs/FLUX.1-schnell"
 
+hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+print("토큰 로드 여부:", hf_token is not None)
 print("Loading model...")
+
 pipe = FluxPipeline.from_pretrained(
     MODEL_ID,
-    torch_dtype=torch.float32
+    torch_dtype=torch.float32,
+    token=hf_token
 )
 
 pipe = pipe.to("cpu")
@@ -27,9 +38,9 @@ start = time.time()
 image = pipe(
     prompt,
     guidance_scale=0.0,
-    num_inference_steps=4,
-    height=768,
-    width=768,
+    num_inference_steps=2,
+    height=512,
+    width=512,
 ).images[0]
 
 image.save("flux_output.png")
